173d5eecd5700eb62bb4b2e35b589d98359edfcc
William Ribeiro <rybeyro93@gmail.com>
2022-01-21 20:52:10 -0300

    add sqlite to save and list data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import dotenv
+import sqlite3
 
 
 dotenv.load_dotenv()
@@ -8,6 +9,19 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 DEBUG = os.getenv("DEBUG")
 
 client = discord.Client()
+
+DB_NAME = "questions"
+connection =  sqlite3.connect(f"{DB_NAME}.db")
+cursor = connection.cursor()
+
+cursor.execute(f"""
+CREATE TABLE IF NOT EXISTS {DB_NAME}(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question VARCHAR,
+    creator VARCHAR,
+    date VARCHAR
+)
+""")
 
 def parse_message(message):
     command_list = ["-list","-add","-help", "-edit"]
@@ -26,7 +40,7 @@ def parse_message(message):
             if(command_ == "-add"):
                 message = validate_data(message[1])
             elif(command_ == "-list"):
-                pass
+                message = list_all_questions()
             elif(command_ == "-edit"):
                 message = validate_data(message)
             else:
@@ -34,6 +48,7 @@ def parse_message(message):
         except Exception as error :
             print(error)
             if DEBUG : message = f"DEBUG: {error}"
+            else: message = "Oops, o programador estava com muita preguiça de validar essa exeção, qualquer duvida é só olhar `$ask -help`"
 
         return message
 
@@ -50,7 +65,10 @@ def validate_data(message, id=None):
         date = message[1]
 
     if check_if_date_is_valid(date):
-        # add logic to save and sotrange data
+        cursor.execute(f"""
+        INSERT INTO {DB_NAME} (question, date) VALUES (?, ?)
+        """, (question, date))
+        connection.commit()
         return f"Sua quetion:\"{question}\" foi salva com suceso para o dia: {date}"
     else:
         return "Não foi possivel salvar sua question porque a data é invalida"
@@ -60,11 +78,12 @@ def add_question():
     date = ""
 
 def list_all_questions():
-    id_ = 0
-    creator = ""
-    question = ""
-    date = ""
-    return {"id": id_, "creator": creator, "question": question, "date": date}
+    message = ""
+    cursor.execute(f'SELECT * FROM {DB_NAME}')
+    for line in cursor.fetchall():
+        message += f"id: {line[0]}, question: {line[1]}, date: {line[3]} \n"
+    
+    return message
 
 def see_help():
     text = open(os.path.realpath("src/texts/help.txt"), "r").readlines()
